d527a82a4917f755f3592a32a2ad602ad0b4018b
Simon Meier <iridcode@gmail.com>
2011-05-28 22:33:06 +0200

    extended regression test with unfoldr and base16 encoding
    base16 encoding builder-based versus base16-bytestring-0.1.1.1
      10000 bytes: 4.0 speedup
      64    bytes: 1.6 speedup
    
    using unfoldr to build an [n,n-1,..,0] bytestring
      10000 bytes: 1.1 speedup
      64    bytes: 1.3 speedup  (effect of different alloc strategy)

--- tests/builder/Regression.hs
+++ tests/builder/Regression.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE PackageImports, CPP, BangPatterns, MonoPatBinds #-}
+{-# LANGUAGE PackageImports #-}
 -- |
 -- Copyright   : (c) 2010-2011 Simon Meier
 -- License     : BSD3-style (see LICENSE)
@@ -15,8 +15,10 @@ import qualified "blaze-builder" Blaze.ByteString.Builder as Blaze
 
 import qualified "new-bytestring" Data.ByteString.Lazy    as NewL
 import qualified "new-bytestring" Data.ByteString.Builder as NewL
+import qualified "new-bytestring" Data.ByteString.Builder.Char.Utf8 as NewL
 
 import qualified "bytestring" Data.ByteString.Lazy as OldL
+import qualified Data.ByteString.Base16.Lazy       as OldBase16
 
 import Foreign
 import Criterion.Main
@@ -52,38 +54,82 @@ lbsInput =
     longLBS  = (OldL.pack word8s, NewL.pack word8s)
     shortLBS = (OldL.pack shortWord8s, NewL.pack shortWord8s)
 
+{-# NOINLINE intInput #-}
+intInput :: [(String, Int)]
+intInput = [ (show n, n), (show nShort, nShort) ]
+
+countToZero :: Int -> Maybe (Word8, Int)
+countToZero 0 = Nothing
+countToZero i = Just (fromIntegral i, i - 1)
+
 
 ------------------------------------------------------------------------------
 -- Main
 ------------------------------------------------------------------------------
 
-newBS, oldBS, blaze :: String
-newBS = "new-bytestring"
-oldBS = "old-bytestring"
-blaze = "blaze"
+newBS :: ([Char], NewL.ByteString -> Int64, NewL.ByteString -> [Word8])
+newBS = ("new-bytestring",  NewL.length, NewL.unpack)
 
-main :: IO ()
-main = defaultMain
-    [ benchmark "filter ((0 ==) . (`mod` 2)) on a LBS"
-        [ (newBS, NewL.length . NewL.filter ((0 ==) . (`mod` 2)) . snd)
-        , (oldBS, OldL.length . OldL.filter ((0 ==) . (`mod` 2)) . fst)
+oldBS :: ([Char], OldL.ByteString -> Int64, OldL.ByteString -> [Word8])
+oldBS = ("old-bytestring",  OldL.length, OldL.unpack)
+
+base16BS :: ([Char], OldL.ByteString -> Int64, OldL.ByteString -> [Word8])
+base16BS = ("base16-bytestring",  OldL.length, OldL.unpack)
+
+blaze :: ([Char], Blaze.Builder -> Int64, Blaze.Builder -> [Word8])
+blaze = ( "blaze"
+        , OldL.length . Blaze.toLazyByteString
+        , OldL.unpack . Blaze.toLazyByteString )
+
+benchmarks :: [Benchmark]
+testResults :: [String]
+(benchmarks, testResults) = unzip $ 
+    [ comparison "unfoldr countToZero starting from" $
+        [ impl newBS (NewL.unfoldr countToZero) intInput
+        , impl oldBS (OldL.unfoldr countToZero) intInput
         ]
-        lbsInput
-    , benchmark "map (+1) on a LBS"
-        [ (newBS, NewL.length . NewL.map (+1) . snd)
-        , (oldBS, OldL.length . OldL.map (+1) . fst)
+     , comparison "base16 encoding of a LBS" $
+        [ impl newBS (NewL.toLazyByteString . NewL.hexLower . snd) lbsInput
+        , impl base16BS (OldBase16.encode . fst) lbsInput
         ]
-        lbsInput
-    , benchmark ("packing [Word8] " ++ show n)
-        [ (newBS, NewL.length . NewL.pack)
-        , (oldBS, OldL.length . OldL.pack)
-        , (blaze, OldL.length . Blaze.toLazyByteString . Blaze.fromWord8s)
+    , comparison "filter ((0 ==) . (`mod` 2)) on a LBS"
+        [ impl newBS (NewL.filter ((0 ==) . (`mod` 2)) . snd) lbsInput
+        , impl oldBS (OldL.filter ((0 ==) . (`mod` 2)) . fst) lbsInput
         ]
-        word8Input
+    , comparison "map (+1) on a LBS"
+        [ impl newBS (NewL.map (+1) . snd) lbsInput
+        , impl oldBS (OldL.map (+1) . fst) lbsInput
+        ]
+    , comparison "pack [Word8]"
+        [ impl newBS NewL.pack word8Input
+        , impl oldBS OldL.pack word8Input
+        , impl blaze Blaze.fromWord8s word8Input
+        ]
     ]
   where
-    benchmark gname tasks xs =
-        bgroup gname [ bgroup xname $ map (mkBench x) tasks | (xname, x) <- xs ]
+    comparison :: Eq d => String -> [([Benchmark], [d])] -> (Benchmark, String)
+    comparison name xs =
+        ( bgroup name $ concat bs
+        , (if success then "OK" else "FAIL" ) ++ ". " ++ name
+        )
       where
-        mkBench x (name, f) = bench name $ whnf f x
+        (bs, ts) = unzip xs
+        success  = and $ zipWith (==) ts $ tail ts
+
+    impl :: (String, b -> c, b -> d) -> (a -> b) -> [(String, a)] 
+          -> ([Benchmark], [d])
+    impl (name, fBench, fTest) f inputs = 
+      ( do (inputName, x) <- inputs
+           return $ bgroup name [bench inputName $ whnf (fBench . f) x]
+      , map (fTest . f . snd) inputs
+      )
+
+
+main :: IO ()
+main = do
+  putStrLn ""
+  putStrLn "Comparing results of different implementations:"
+  putStrLn $ unlines $ map ("  " ++) testResults
+  putStrLn "Benchmarking:"
+  defaultMain benchmarks
 

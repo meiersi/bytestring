628dbccf0cf5c3af224b881ffbbbbc4c3743e366
Simon Meier <iridcode@gmail.com>
2011-06-20 23:15:58 +0200

    designed set of rewriting rules to optimize Put monad expressions

--- tests/builder/Test_RewriteFoldr.hs
+++ tests/builder/Test_RewriteFoldr.hs
@@ -2,37 +2,36 @@
 -- Created: 2011 06 14
 -- Author:  Simon Meier <iridcode@gmail.com>
 --
--- Testcase for rewriting rules that involve 'foldr'. The testcase is
--- motivated by the Builder being implemented for the bytestring library.
--- There, we would like to exploit equations like
---
---   mconcat . map (fromWrite w) = fromWriteList w
---
--- in order to gain additional (2x - 10x) more efficiency. However, I could not
--- formulate a rewriting rule that matches the appropriate intermediate form of
--- 'mconcat . map (fromWrite w)'. The following definitions illustrate the
--- problem with the same names but simpler types.
---
--- See
--- https://github.com/meiersi/bytestring/blob/master/Data/ByteString/Builder/Write.hs 
--- for more context on Writes and Builders.
+-- Testcase for rewriting rules on Builders and Puts.
 module Test_RewriteFoldr where
 
 import Data.Monoid
 import Data.List
 import qualified Data.Foldable as F
+import Control.Applicative hiding (empty)
 
 import GHC.Base
 
+------------------------------------------------------------------------------
+-- Utilities
+------------------------------------------------------------------------------
+
+-- | Observable append of non-empty 'String's.
+obsAppend :: String -> String -> String
+obsAppend x  [] = x
+obsAppend [] y  = y
+obsAppend x  y  = x ++ " | " ++ y
+
+------------------------------------------------------------------------------
+-- Builders
+------------------------------------------------------------------------------
 
 newtype Builder = Builder String
   deriving( Show )
 
 {-# INLINE [1] append #-}
 append :: Builder -> Builder -> Builder
-append x            (Builder "") = x
-append (Builder "") y            = y
-append (Builder x ) (Builder y)  = Builder (x ++ " | " ++ y)
+append (Builder x ) (Builder y)  = Builder (obsAppend x y)
 
 {-# INLINE [1] empty #-}
 empty  :: Builder
@@ -46,20 +45,105 @@ instance Monoid Builder where
   {-# INLINE mconcat #-}
   mconcat = foldr mappend mempty
 
+-- Late "application" of missing INLINE pragma on default implementation of
+-- 'foldMap' from 'Foldable'.
+{-# RULES "Specialize foldMap_Builder" F.foldMap = foldMap_Builder #-}
+{-# INLINE foldMap_Builder #-}
+foldMap_Builder :: F.Foldable f => (a -> Builder) -> f a -> Builder
+foldMap_Builder f = F.foldr (mappend . f) mempty
+
+
+------------------------------------------------------------------------------
+-- Puts
+------------------------------------------------------------------------------
+
+data Put a = Put a String
+  deriving( Show )
+
+instance Functor Put where
+  {-# INLINE fmap #-}
+  fmap f (Put x cs) = Put (f x) cs
+
+{-# INLINE [1] bind_Put #-}
+bind_Put :: Put a -> (a -> Put b) -> Put b
+bind_Put (Put x cs) f = 
+    let Put x' cs' = f x in Put x' (cs `obsAppend` cs')
+
+{-# INLINE [1] left_result_Put #-}
+left_result_Put :: Put a -> Put b -> Put a
+left_result_Put (Put a acs) (Put _ bcs) = Put a (acs `obsAppend` bcs)
+
+{-# INLINE [1] right_result_Put #-}
+right_result_Put :: Put a -> Put b -> Put b
+right_result_Put (Put _ acs) (Put b bcs) = Put b (acs `obsAppend` bcs)
+
+instance Applicative Put where
+  {-# INLINE pure #-}
+  pure = return
+  {-# INLINE (<*>) #-}
+  Put f fcs <*> Put a acs = Put (f a) (fcs `obsAppend` acs)
+  (<*) = left_result_Put
+  (*>) = right_result_Put
+
+instance Monad Put where
+  {-# INLINE return #-}
+  return x = Put x ""
+  {-# INLINE (>>=) #-}
+  (>>=) = bind_Put
+  {-# INLINE (>>) #-}
+  (>>) = right_result_Put
+
+{-# INLINE putBuilder #-}
+putBuilder :: Builder -> Put ()
+putBuilder (Builder cs) = Put () cs
+
+{-# INLINE fromPut #-}
+fromPut :: Put () -> Builder
+fromPut (Put _ cs) = Builder cs
+
+{-# RULES
+"Put/*>" forall m n.
+      m `right_result_Put` n
+    = putBuilder (fromPut m `append` fromPut n)
+
+"Put/<*" forall m n.
+      m `left_result_Put` n
+    = putBuilder (fromPut m `append` fromPut n)
+ #-}
+
+------------------------------------------------------------------------------
+-- Writes
+------------------------------------------------------------------------------
+
 -- | Writes are primitive serialization functions.
 data Write a = Write (a -> String)
 
 (#.) :: Write a -> (b -> a) -> Write b
 (Write w) #. f = Write (w . f)
 
+write2 :: Write a -> Write b -> Write (a, b)
+write2 (Write w1) (Write w2) = Write $ \(x,y) -> w1 x ++ " .2. " ++ w2 y
+
 -- | An dummy implementation of a write for strings.
 writeString :: Write String
 writeString = Write id
+
+fromString :: String -> Builder
+fromString = fromWrite writeString
+
+putString :: String -> Put ()
+putString = putBuilder . fromString
+
 
 -- | Writes can be used to construct builders for singleton arguments...
 {-# INLINE [1] fromWrite #-}
 fromWrite :: Write a -> a -> Builder
 fromWrite (Write f) x = Builder ("write: " ++ f x)
+
+{-# RULES "append/fromWrite" forall w1 w2 x1 x2.
+       append (fromWrite w1 x1) (fromWrite w2 x2) 
+     = fromWrite (write2 w1 w2) (x1, x2)
+  #-}
 
 -- | ...as well as for lists. Here, we mark what 'fromWriteXXX' function was
 -- used to detect if a rewriting rule was applied or not.
@@ -77,7 +161,8 @@ fromWriteList (Write f) xs =
 -- 'fromWriteList' version.
 {-# RULES
 "foldr/fromWrite" forall w.
-    foldr (\x -> append (fromWrite w x)) empty = fromWriteList w 
+    foldr (\x -> append (fromWrite w x)) empty 
+  = fromWriteList w 
 
 "foldr/fromWrite/#." forall w f b.
     foldr (\x -> append (fromWrite w (f x))) b
@@ -92,10 +177,10 @@ fromWriteList (Write f) xs =
  #-}
 
 
--- All of the following expressions should yield the same result when compiling
--- with -O2. However, only the first two do, while the 'fails' expression gets
--- probably converted to some intermediate representation that the rule doesn't
--- match on.
+------------------------------------------------------------------------------
+-- Test cases
+------------------------------------------------------------------------------
+
 goal :: [String] -> Builder
 goal = fromWriteList writeString
 
@@ -108,29 +193,33 @@ works2 =  mconcat . map (fromWrite writeString)
 works3 :: [String] -> Builder
 works3 =  mconcat . map (fromWrite writeString) . map reverse
 
--- Late "application" of missing INLINE pragma on default implementation of
--- 'foldMap' from 'Foldable'.
-{-# RULES "Specialize foldMap_Builder" F.foldMap = foldMap_Builder #-}
-{-# INLINE foldMap_Builder #-}
-foldMap_Builder :: F.Foldable f => (a -> Builder) -> f a -> Builder
-foldMap_Builder f = F.foldr (mappend . f) mempty
-
-
 works4 :: [String] -> Builder
 works4 = foldMap_Builder $ fromWrite writeString
 
 works5 :: [String] -> Builder
 works5 = F.foldMap $ fromWrite writeString
 
--- foldr (mappend . fromWrite writeString) mempty = 
--- foldr (append . fromWrite writeString) empty = 
--- foldr (\x -> append (fromWrite writeString x)) empty = 
+-- Is not optimized as well as the other cases: interchangeability of mapM_ and
+-- putBuilder is to hard for the compiler.
+test6 :: [String] -> Builder
+test6 = fromPut . mapM_ (putBuilder . fromWrite writeString)
+
+test7 :: [String] -> Builder
+test7 = F.foldMap (\x -> fromString x `mappend` fromString (tail x))
+
+-- This case is rather nice: the triple put's are fused together and would
+-- share a single bound-check in the actual implementation.
+test8 :: [String] -> Builder
+test8 (x:y:z:_) = fromPut $ do {putString x; putString y; putString z}
+
+test9 :: [String] -> Builder
+test9 (x:y:_) = fromWrite (write2 writeString writeString) (x, y)
 
 
 main :: IO ()
 main = do
-    putStrLn "The following three results should all use 'fromWriteList':"
-    mapM_ (print . ($ input)) [goal, works, works2, works3, works4, works5]
+    mapM_ (print . ($ input)) [ goal, works, works2, works3, works4, works5
+                              , test6, test7, test8, test9 ]
   where
     input :: [String]
     input = ["hello", "world", "!"]

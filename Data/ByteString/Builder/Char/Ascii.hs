9a26dcf8b5b59c574dd6316b21d809e70bdc5134
Simon Meier <iridcode@gmail.com>
2011-06-20 09:10:17 +0200

    finished ASCII and UTF-8 encodings
      - documentation
      - introduced separate type-class for no-leading-zeros hex-encodings.
        (added provisional 'Integer' instance)

--- Data/ByteString/Builder/Char/Ascii.hs
+++ Data/ByteString/Builder/Char/Ascii.hs
@@ -1,10 +1,17 @@
--- | Copyright : (c) 2010      Jasper Van der Jeugt 
---               (c) 2010-2011 Simon Meier
+-- | Copyright : (c) 2011 Simon Meier
 -- License     : BSD3-style (see LICENSE)
 -- 
 -- Maintainer  : Simon Meier <iridcode@gmail.com>
 -- Stability   : experimental
 -- Portability : tested on GHC only
+--
+-- Functions for creating 'Builder's by ASCII encoding characters
+-- (cf. <http://tools.ietf.org/html/rfc20>).
+--
+-- They are intended for constructing output in formats that explicitly
+-- restrict the character encoding to 7-bit ASCII encoded characters. In all
+-- other cases, a lossless Unicode encoding (e.g.,
+-- "Data.ByteString.Builder.Char.Utf8") is a better choice.
 --
 module Data.ByteString.Builder.Char.Ascii
     ( 
@@ -12,11 +19,12 @@ module Data.ByteString.Builder.Char.Ascii
       Ascii(..)
 
       -- * Hexadecimal encoding using ASCII encoded characters
-    , asciiHexLowerNoLead
-    , asciiHexUpperNoLead
     , AsciiHex(..)
+    , AsciiHexNoLead(..)
     ) where
 
+import Numeric       (showHex)
+import Data.Char     (toUpper)
 import Data.Foldable (foldMap)
 
 import Data.ByteString.Builder.Internal
@@ -24,22 +32,74 @@ import Data.ByteString.Builder.Write
 
 import qualified System.IO.Write.Char.Ascii as W
 
+
 import Foreign
 
-asciiHexLowerNoLead :: W.AsciiHexWritable a => a -> Builder
-asciiHexLowerNoLead = fromWrite W.asciiHexLowerNoLead
-
-asciiHexUpperNoLead :: W.AsciiHexWritable a => a -> Builder
-asciiHexUpperNoLead = fromWrite W.asciiHexUpperNoLead
-
+-- | ASCII encoding of character(s) with a Unicode codepoint below 128. The
+-- class methods differ in how non-encable characters with a codepoint greater
+-- or equal to 128 are handled.
 class Ascii a where
+    -- | Throws an error if there is a non-encodable character.
     ascii        :: a -> Builder
+
+    -- | Writes the given replacement character for non-encodable characters.
+    -- The replacement character must have a codepoint below 128. Otherwise
+    -- an error is thrown.
     asciiReplace :: Char -> a -> Builder
+
+    -- | Non-encodable characters are ignored.
+    --
+    -- Note that dropping characters during encoding is often dangerous, as it
+    -- may lead to unintended interpretations of the written output. Either use
+    -- 'asciiReplace' to insert a character whose insertion is safe in your
+    -- context or, if possible, use a lossless encoding like UTF-8.
     asciiDrop    :: a -> Builder
 
+
+-- | Values that support a fixed-width hexadecimal encoding using ASCII encoded
+-- characters.
+class AsciiHex a where
+    -- | Fixed-width hexadecimal encoding with lower-case characters.
+    --
+    -- > toLazyByteString (asciiHexLower [26 :: Word16, 15]) = pack "001a000f"
+    --
+    asciiHexLower :: a -> Builder
+
+    -- | Fixed-width hexadecimal encoding with upper-case characters.
+    --
+    -- > toLazyByteString (asciiHexLower [26 :: Word16, 15]) = pack "001A000F"
+    --
+    asciiHexUpper :: a -> Builder
+
+
+-- | Values that support a hexadecimal encoding with no leading zeros using
+-- ASCII encoded characters.
+--
+-- Note that there is no instance for lists or other sequence-like types, as
+-- concatenating the encodings of a sequence's elements results in a
+-- non-injective (i.e., ambiguous) function.
+class AsciiHexNoLead a where
+    -- | Hexadecimal encoding with lower-case characters and no leading zeros.
+    --
+    -- > toLazyByteString (asciiHexLowerNoLead (26 :: Word16)) = "1a"
+    --
+    asciiHexLowerNoLead :: a -> Builder
+
+    -- | Hexadecimal encoding with upper-case characters and no leading zeros.
+    --
+    -- > toLazyByteString (asciiHexLowerNoLead (26 :: Word16)) = "1A"
+    --
+    asciiHexUpperNoLead :: a -> Builder
+
+
+-- Ascii instances
+
 instance Ascii Char where
+    {-# INLINE ascii #-}
     ascii          = fromWrite W.ascii
+    {-# INLINE asciiReplace #-}
     asciiReplace c = fromWrite (W.asciiReplace c)
+    {-# INLINE asciiDrop #-}
     asciiDrop      = fromWrite W.asciiDrop
 
 instance Ascii a => Ascii [a] where
@@ -50,10 +110,13 @@ instance Ascii a => Ascii [a] where
     {-# INLINE asciiDrop #-}
     asciiDrop      = foldMap asciiDrop
 
+-- AsciiHex instances
 
-class AsciiHex a where
-    asciiHexLower :: a -> Builder
-    asciiHexUpper :: a -> Builder
+instance AsciiHex a => AsciiHex [a] where
+    {-# INLINE asciiHexLower #-}
+    asciiHexLower = foldMap asciiHexLower
+    {-# INLINE asciiHexUpper #-}
+    asciiHexUpper = foldMap asciiHexUpper
 
 instance AsciiHex Word where
     {-# INLINE asciiHexLower #-}
@@ -115,4 +178,70 @@ instance AsciiHex Int64 where
     {-# INLINE asciiHexUpper #-}
     asciiHexUpper = fromWrite W.asciiHexUpper
 
+
+-- AsciiHexNoLead instances
+
+instance AsciiHexNoLead Integer where
+    asciiHexLowerNoLead i = ascii               $ showHex i ""
+    asciiHexUpperNoLead i = ascii $ map toUpper $ showHex i ""
+
+instance AsciiHexNoLead Word where
+    {-# INLINE asciiHexLowerNoLead #-}
+    asciiHexLowerNoLead = fromWrite W.asciiHexLowerNoLead
+    {-# INLINE asciiHexUpperNoLead #-}
+    asciiHexUpperNoLead = fromWrite W.asciiHexUpperNoLead
+
+instance AsciiHexNoLead Word8 where
+    {-# INLINE asciiHexLowerNoLead #-}
+    asciiHexLowerNoLead = fromWrite W.asciiHexLowerNoLead
+    {-# INLINE asciiHexUpperNoLead #-}
+    asciiHexUpperNoLead = fromWrite W.asciiHexUpperNoLead
+
+instance AsciiHexNoLead Word16 where
+    {-# INLINE asciiHexLowerNoLead #-}
+    asciiHexLowerNoLead = fromWrite W.asciiHexLowerNoLead
+    {-# INLINE asciiHexUpperNoLead #-}
+    asciiHexUpperNoLead = fromWrite W.asciiHexUpperNoLead
+
+instance AsciiHexNoLead Word32 where
+    {-# INLINE asciiHexLowerNoLead #-}
+    asciiHexLowerNoLead = fromWrite W.asciiHexLowerNoLead
+    {-# INLINE asciiHexUpperNoLead #-}
+    asciiHexUpperNoLead = fromWrite W.asciiHexUpperNoLead
+
+instance AsciiHexNoLead Word64 where
+    {-# INLINE asciiHexLowerNoLead #-}
+    asciiHexLowerNoLead = fromWrite W.asciiHexLowerNoLead
+    {-# INLINE asciiHexUpperNoLead #-}
+    asciiHexUpperNoLead = fromWrite W.asciiHexUpperNoLead
+
+instance AsciiHexNoLead Int where
+    {-# INLINE asciiHexLowerNoLead #-}
+    asciiHexLowerNoLead = fromWrite W.asciiHexLowerNoLead
+    {-# INLINE asciiHexUpperNoLead #-}
+    asciiHexUpperNoLead = fromWrite W.asciiHexUpperNoLead
+
+instance AsciiHexNoLead Int8 where
+    {-# INLINE asciiHexLowerNoLead #-}
+    asciiHexLowerNoLead = fromWrite W.asciiHexLowerNoLead
+    {-# INLINE asciiHexUpperNoLead #-}
+    asciiHexUpperNoLead = fromWrite W.asciiHexUpperNoLead
+
+instance AsciiHexNoLead Int16 where
+    {-# INLINE asciiHexLowerNoLead #-}
+    asciiHexLowerNoLead = fromWrite W.asciiHexLowerNoLead
+    {-# INLINE asciiHexUpperNoLead #-}
+    asciiHexUpperNoLead = fromWrite W.asciiHexUpperNoLead
+
+instance AsciiHexNoLead Int32 where
+    {-# INLINE asciiHexLowerNoLead #-}
+    asciiHexLowerNoLead = fromWrite W.asciiHexLowerNoLead
+    {-# INLINE asciiHexUpperNoLead #-}
+    asciiHexUpperNoLead = fromWrite W.asciiHexUpperNoLead
+
+instance AsciiHexNoLead Int64 where
+    {-# INLINE asciiHexLowerNoLead #-}
+    asciiHexLowerNoLead = fromWrite W.asciiHexLowerNoLead
+    {-# INLINE asciiHexUpperNoLead #-}
+    asciiHexUpperNoLead = fromWrite W.asciiHexUpperNoLead
 
--- Data/ByteString/Builder/Char/Utf8.hs
+++ Data/ByteString/Builder/Char/Utf8.hs
@@ -6,9 +6,11 @@
 -- Stability   : experimental
 -- Portability : tested on GHC only
 --
+-- Functions for creating 'Builder's by UTF-8 encoding characters.
+--
 module Data.ByteString.Builder.Char.Utf8
     ( 
-      -- * UTF-8 encoded characters
+      -- * UTF-8 encoding characters
       Utf8(..)
 
       -- * Hexadecimal encoding using UTF-8 encoded characters
@@ -24,11 +26,12 @@ import Data.ByteString.Builder.Internal
 import Data.ByteString.Builder.Write
 import Data.ByteString.Builder.Char.Ascii
 
-import qualified System.IO.Write.Char.Ascii as W
 import qualified System.IO.Write.Char.Utf8  as W
 
 
+-- | UTF-8 encodable values.
 class Utf8 a where
+    -- | Encode using UTF-8.
     utf8 :: a -> Builder
 
 instance Utf8 Char where
@@ -39,14 +42,40 @@ instance Utf8 a => Utf8 [a] where
     {-# INLINE utf8 #-}
     utf8 = foldMap utf8
 
+-- | Fixed-width hexadecimal encoding with lower-case characters encoded using
+-- UTF-8.
+--
+-- > toLazyByteString (utf8HexLower [26 :: Word16, 15]) = pack "001a000f"
+--
+-- Note that we exploit that the UTF-8 encoding coincides with the ASCII
+-- encoding on codepoints below 128. This is the origin of the
+-- 'AsciiHex' type-class constraint.
+--
 utf8HexLower :: AsciiHex a => a -> Builder
 utf8HexLower = asciiHexLower
 
+-- | Fixed-width hexadecimal encoding with upper-case characters encoded using
+-- UTF-8.
+--
+-- > toLazyByteString (utf8HexLower [26 :: Word16, 15]) = pack "001A000F"
+--
 utf8HexUpper :: AsciiHex a => a -> Builder
 utf8HexUpper = asciiHexUpper
 
-utf8HexLowerNoLead :: W.AsciiHexWritable a => a -> Builder
+
+-- | Hexadecimal encoding with no leading zeros and lower-case characters 
+-- encoded using UTF-8.
+--
+-- > toLazyByteString (utf8HexLowerNoLead (26 :: Word16)) = "1a"
+--
+utf8HexLowerNoLead :: AsciiHexNoLead a => a -> Builder
 utf8HexLowerNoLead = asciiHexLowerNoLead
 
-utf8HexUpperNoLead :: W.AsciiHexWritable a => a -> Builder
+-- | Hexadecimal encoding with no leading zeros and upper-case characters
+-- encoded using UTF-8.
+--
+-- > toLazyByteString (utf8HexUpperNoLead (26 :: Word16)) = "1A"
+--
+utf8HexUpperNoLead :: AsciiHexNoLead a => a -> Builder
 utf8HexUpperNoLead = asciiHexUpperNoLead
+

d20edcad30d75e45a5a91def18bbb870edb659c7
Simon Meier <iridcode@gmail.com>
2011-06-27 16:56:12 +0200

    running example for Builder documentation

--- Data/ByteString/Builder.hs
+++ Data/ByteString/Builder.hs
@@ -8,9 +8,186 @@
 -- Stability   : experimental
 -- Portability : tested on GHC only
 --
--- TODO: Intro and example
+-- The 'Builder' type provides an efficient representation of a stream of bytes
+-- (i.e, a possibly infinite sequence of bytes). It is efficient in the following sense:
+--
+--   1. The stream of bytes represented by a 'Builder' can be accessed as a
+--      lazy 'L.ByteString' whose chunks are large on average.
+--
+--   2. Concatenating two 'Builder's is in /O(1)/.
+--
+--   3. Encoding values of standard Haskell types like 'Int32' or 'Char' as a
+--      sequence of bytes represented as a 'Builder' is efficient in terms of
+--      CPU cycles spent.
+--
+-- Property (1) guarantees that processing the resulting lazy 'L.ByteString'
+-- chunk-wise is efficient, as the work on the chunk boundaries can be
+-- amortized by a significant amount of actual work spent on the chunk content.
+-- For example, when writing the resulting lazy 'L.ByteString' to a file or to a
+-- network socket, the large chunk size guarantees that the cost of a system
+-- call is distributed over enough bytes being written. Property (2)
+-- guarantees that 'Builder's can be assembled efficiently from smaller
+-- 'Builder's. This makes 'Builder's well-suited for declaratively describing a
+-- stream of bytes. It is also is a significant difference to strict and lazy
+-- bytestrings, whose concatenation is in /O(n)/. Property (3) guarantees that
+-- creating the primitive 'Builder's, from which we assemble our 'Builder's of
+-- interest, is efficent in terms of wall time.
+--
+-- Typically, 'Builder's are used to encode application internal data as a
+-- sequence of bytes in an external format like JSON, HTML, or a HTTP response.
+-- As a simple example, we'll show how to encode the following representation
+-- of mixed-data tables as a Comma-Separated-Values (CSV) file. (Note that the
+-- whole example is distributed with the source of this library.)
+--
+-- > data Cell = StringC String
+-- >           | IntC Int
+-- >           deriving( Eq, Ord, Show )
+-- > 
+-- > type Row   = [Cell]
+-- > type Table = [Row]
+--
+-- We use the following imports and abbreviate 'mappend' to simplify reading.
+--
+-- > import qualified Data.ByteString.Lazy      as L
+-- > import qualified Data.ByteString.Builder   as B
+-- > import           Data.Monoid
+-- > import           Data.List                 (intersperse)
+-- > 
+-- > infixr 4 <>
+-- > (<>) :: Monoid m => m -> m -> m
+-- > (<>) = mappend
+--
+-- CSV is a character-based representation of tables. For maximal modularity,
+-- we'd first render 'Table's as 'String's and then encode this 'String' to a
+-- file using some fixed character encoding. However, this sacrifices performance
+-- due to the intermediate 'String' representation being built and thrown away
+-- right afterwards. We get rid of this intermediate 'String' representation by
+-- fixing the character encoding to UTF-8 and using 'Builder's to convert
+-- 'Table's directly to UTF-8 encoded CSV tables represented as lazy
+-- 'L.ByteString's.
+--
+-- > encodeUtf8CSV :: Table -> L.ByteString
+-- > encodeUtf8CSV = B.toLazyByteString . renderTable
+-- > 
+-- > renderTable :: Table -> B.Builder
+-- > renderTable rs = mconcat [renderRow r <> B.utf8 '\n' | r <- rs]
+-- >            
+-- > renderRow :: Row -> B.Builder
+-- > renderRow []     = mempty
+-- > renderRow (c:cs) = 
+-- >     renderCell c <> mconcat [ B.utf8 ',' <> renderCell c' | c' <- cs ]
+-- > 
+-- > renderCell :: Cell -> B.Builder
+-- > renderCell (StringC cs) = renderString cs
+-- > renderCell (IntC i)     = B.utf8 $ show i
+-- > 
+-- > renderString :: String -> B.Builder
+-- > renderString cs = B.utf8 '"' <> B.foldMap escape cs <> B.utf8 '"'
+-- >   where
+-- >     escape '\\' = B.utf8 '\\' <> B.utf8 '\\'
+-- >     escape '\"' = B.utf8 '\\' <> B.utf8 '\"'
+-- >     escape c    = B.utf8 c
+--
+-- We use our UTF-8 CSV encoding function on the following table. /Note that/
+-- /Haddock currently doesn't handle the Unicode lambda-character and the/
+-- /o-umlaut character in the last string "<lambda>-w<o-umlaut>rld". They are/
+-- /present in the source, but not output by Haddock./
+--
+-- > strings :: [String]
+-- > strings =  ["hello", "\"1\"", "λ-wörld"]
+-- > 
+-- > table :: Table
+-- > table = [map StringC strings, map IntC [-3..3]]
+--
+-- The expression @encodeUtf8CSV table@ results in the following lazy
+-- 'L.ByteString'.
+--
+-- > Chunk "\"hello\",\"\\\"1\\\"\",\"\206\187-w\195\182rld\"\n-3,-2,-1,0,1,2,3\n" Empty
+--
+-- We can clearly see that we are converting to a /binary/ format. The \'λ\'
+-- (lambda-character) and \'ö\' (o-umlaut) characters, which have a Unicode
+-- codepoint above 127, are expanded to their corresponding UTF-8 multi-byte
+-- representation.
+--
+-- We use the criterion library to benchmark the efficiency of our encoding
+-- function on the following table.
+--
+-- > import Criterion.Main     -- add this import to the ones above
+-- >
+-- > maxiTable :: Table
+-- > maxiTable = take 1000 $ cycle table
+-- >
+-- > main :: IO ()
+-- > main = defaultMain
+-- >   [ bench "encodeUtf8CSV maxiTable (original)" $ 
+-- >       whnf (L.length . encodeUtf8CSV) maxiTable
+-- >   ]
+--
+-- On a Core2 Duo 2.20GHz on a 32-bit Linux, the above code takes 1.1ms to
+-- generate the 22'500 bytes long lazy 'L.ByteString'. Looking again at
+-- the definitions above, we see that we took care to avoid intermediate
+-- data structures, as otherwise we would sacrifice performance.
+-- For example, the following (arguably simpler) definition of 'renderRow' is
+-- about 20% slower.
+--
+-- > renderRow :: Row -> B.Builder
+-- > renderRow  = mconcat . intersperse (B.utf8 ',') . map renderCell
+--
+-- Similarly, using /O(n)/ concatentations like '(++)' should be avoided. The
+-- following definition of 'renderString' is also about 20% slower.
+--
+-- > renderString :: String -> B.Builder
+-- > renderString cs = B.utf8 $ "\"" ++ concatMap escape cs ++ "\"" 
+-- >   where
+-- >     escape '\\' = "\\"
+-- >     escape '\"' = "\\\""
+-- >     escape c    = return c
+--
+-- Note that on the above example 'Builder's are not much faster than using
+-- difference-lists to achieve efficient 'String' concatenation. Rendering the
+-- table first as a 'String' using a difference-list and UTF-8 encoding it
+-- afterwards takes 1.3ms. However, this performance difference becomes more
+-- pronounced once longer snippets of static-data are involved. 'Builder's can
+-- exploit low-level memory copy and write operations, while 'String's always
+-- have to follow one pointer per 'Char' and process this 'Char' individually.
+--
+-- Apart from removing intermediate data-structures, 'Builder's can be
+-- optimized further by exploiting knowledge about their implementation.
+-- Internally, 'Builder's are buffer-fill operations that are
+-- given a continuation buffer-fill operation and a buffer-range to be filled.
+-- A 'Builder' first checks if the buffer-range is large enough. If that's
+-- the case, the 'Builder' writes the sequences of bytes to the buffer and
+-- calls its continuation.  Otherwise, it returns a signal that it requires a
+-- new buffer together with a continuation to be called on this new buffer.
+-- Ignoring the rare case of a full buffer-range, the execution cost of a
+-- 'Builder' consists of three parts: 
+--
+--   1. The time taken to read the parameters; i.e., the buffer-fill
+--      operation to call after the 'Builder' is done and the buffer-range to
+--      fill. 
+--
+--   2. The time taken to check for the size of the buffer-range.
+--
+--   3. The time taken for the actual encoding.
+--
+-- We can reduce cost (1) by ensuring that fewer buffer-fill function calls are
+-- required. We can reduce cost (2) by fusing buffer-size checks of sequential
+-- writes. For example, when escaping a 'String' using 'renderString', it would
+-- be sufficient to check before encoding a character that at least 8 bytes are
+-- free. We can reduce cost (3) by implementing better primitive 'Builder's.
+-- For example, 'renderCell' builds an intermediate list containing the decimal
+-- representation of an 'Int'. Implementing a direct decimal encoding of 'Int's
+-- to memory would be more efficient, as it requires fewer buffer-size checks
+-- and less allocation. It is also a planned extension of this library.
+--
+-- The first two cost reductions are supported for user code through functions
+-- in "Data.ByteString.Builder.Write". There, we continue the above example
+-- and drop the generation time to 0.9ms by implementing 'renderString' more
+-- cleverly. The third reduction requires meddling with the internals of
+-- 'Builder's and is not recomended in code outside of this library. However,
+-- patches to this library are very welcome. 
+--
 -----------------------------------------------------------------------------
-
 module Data.ByteString.Builder
     ( 
       -- * The Builder type
@@ -178,7 +355,7 @@ toLazyByteStringUntrimmed = toLazyByteStringWith
 -- > toLazyByteStringWith (safeStrategy 128 smallChunkSize) empty
 --
 -- This reduces the allocation and trimming overhead, as all generated
--- 'L.ByteString's fit into ther first allocated buffer and chances are better
+-- 'L.ByteString's fit into the first allocated buffer and chances are better
 -- that the buffer doesn't have to be trimmed.
 --
 {-# INLINE toLazyByteStringWith #-}
--- tests/builder/CSV.hs
+++ tests/builder/CSV.hs
@@ -10,9 +10,11 @@
 --
 module CSV where
 
-import qualified Data.ByteString.Lazy              as L
-import Data.ByteString.Builder          
-import Data.ByteString.Builder.Write    
+import qualified Data.ByteString               as S
+import qualified Data.ByteString.Lazy          as L
+import qualified Data.ByteString.Builder       as B
+import qualified Data.ByteString.Builder.Write as B
+import qualified Data.DList                    as D
 
 
 import           System.IO.Write       (Write, writeIf, write2, (#.))
@@ -20,7 +22,8 @@ import qualified System.IO.Write as W  (utf8)
 
 import Criterion.Main
 import Data.Monoid
-import Data.List
+import Data.List (intersperse)
+
 import Data.Int
 
 ------------------------------------------------------------------------------
@@ -28,10 +31,8 @@ import Data.Int
 ------------------------------------------------------------------------------
 
 infixr 4 <>
-
 (<>) :: Monoid m => m -> m -> m
 (<>) = mappend
-
 
 data Cell = StringC String
           | IntC Int
@@ -41,25 +42,25 @@ type Row   = [Cell]
 type Table = [Row]
 
 
-renderString :: String -> Builder
-renderString cs = utf8 $ "\"" ++ concatMap escape cs ++ "\"" 
+renderString :: String -> B.Builder
+renderString cs = B.utf8 $ "\"" ++ concatMap escape cs ++ "\"" 
   where
     escape '\\' = "\\"
     escape '\"' = "\\\""
     escape c    = return c
 
-renderCell :: Cell -> Builder
+renderCell :: Cell -> B.Builder
 renderCell (StringC cs) = renderString cs
-renderCell (IntC i)     = utf8 $ show i
+renderCell (IntC i)     = B.utf8 $ show i
            
-renderRow :: Row -> Builder
-renderRow  = mconcat . intersperse (utf8 ',') . map renderCell
+renderRow :: Row -> B.Builder
+renderRow  = mconcat . intersperse (B.utf8 ',') . map renderCell
 
-renderTable :: Table -> Builder
-renderTable rs = mconcat [renderRow r <> utf8 '\n' | r <- rs]
+renderTable :: Table -> B.Builder
+renderTable rs = mconcat [renderRow r <> B.utf8 '\n' | r <- rs]
 
 encodeUtf8CSV :: Table -> L.ByteString
-encodeUtf8CSV = toLazyByteString . renderTable
+encodeUtf8CSV = B.toLazyByteString . renderTable
 
 strings :: [String]
 strings =  ["hello", "\"1\"", "λ-wörld"]
@@ -67,34 +68,70 @@ strings =  ["hello", "\"1\"", "λ-wörld"]
 table :: Table
 table = [map StringC strings, map IntC [-3..3]]
 
+n :: Int
+n = 1000
+
 maxiTable :: Table
-maxiTable = take 1000 $ cycle table
+maxiTable = take n $ cycle table
+
+------------------------------------------------------------------------------
+-- Difference List based
+------------------------------------------------------------------------------
+
+type DString = D.DList Char
+
+-- improved
+renderStringD :: String -> DString
+renderStringD cs = return '"' <> mconcat (map escape cs) <> return '"'
+  where
+    escape '\\' = D.DL $ \k -> '\\':'\\':k
+    escape '\"' = D.DL $ \k ->  '\\':'"':k
+    escape c    = return c
+
+-- carry over definitions
+renderCellD :: Cell -> DString
+renderCellD (StringC cs) = renderStringD cs
+renderCellD (IntC i)     = D.fromList $ show i
+
+renderRowD :: Row -> DString
+renderRowD []     = mempty
+renderRowD (c:cs) = 
+    renderCellD c <> mconcat [ return ',' <> renderCellD c' | c' <- cs ]
+
+renderTableD :: Table -> DString
+renderTableD rs = mconcat [renderRowD r <> return '\n' | r <- rs]
+
+encodeCSV :: Table -> String
+encodeCSV = D.toList . renderTableD
+
+encodeUtf8CSV_Dlist :: Table -> L.ByteString
+encodeUtf8CSV_Dlist = B.toLazyByteString . B.fromWriteList W.utf8 . encodeCSV
       
 ------------------------------------------------------------------------------
 -- Version 1
 ------------------------------------------------------------------------------
 
 -- improved
-renderString1 :: String -> Builder
-renderString1 cs = utf8 '"' <> foldMap escape cs <> utf8 '"'
+renderString1 :: String -> B.Builder
+renderString1 cs = B.utf8 '"' <> B.foldMap escape cs <> B.utf8 '"'
   where
-    escape '\\' = utf8 '\\' <> utf8 '\\'
-    escape '\"' = utf8 '\\' <> utf8 '\"'
-    escape c    = utf8 c
+    escape '\\' = B.utf8 '\\' <> B.utf8 '\\'
+    escape '\"' = B.utf8 '\\' <> B.utf8 '\"'
+    escape c    = B.utf8 c
 
 -- carry over definitions
-renderCell1 :: Cell -> Builder
+renderCell1 :: Cell -> B.Builder
 renderCell1 (StringC cs) = renderString1 cs
-renderCell1 (IntC i)     = utf8 $ show i
+renderCell1 (IntC i)     = B.utf8 $ show i
            
-renderRow1 :: Row -> Builder
-renderRow1  = mconcat . intersperse (utf8 ',') . map renderCell1
+renderRow1 :: Row -> B.Builder
+renderRow1  = mconcat . intersperse (B.utf8 ',') . map renderCell1
 
-renderTable1 :: Table -> Builder
-renderTable1 rs = mconcat [renderRow1 r <> utf8 '\n' | r <- rs]
+renderTable1 :: Table -> B.Builder
+renderTable1 rs = mconcat [renderRow1 r <> B.utf8 '\n' | r <- rs]
 
 encodeUtf8CSV1 :: Table -> L.ByteString
-encodeUtf8CSV1 = toLazyByteString . renderTable1
+encodeUtf8CSV1 = B.toLazyByteString . renderTable1
 
 
 ------------------------------------------------------------------------------
@@ -102,19 +139,16 @@ encodeUtf8CSV1 = toLazyByteString . renderTable1
 ------------------------------------------------------------------------------
 
 
-renderRow2 :: Row -> Builder
+renderRow2 :: Row -> B.Builder
 renderRow2 []     = mempty
 renderRow2 (c:cs) = 
-    renderCell1 c <> mconcat [ utf8 ',' <> renderCell1 c' | c' <- cs ]
-    -- renderCell1 c <> foldr (\c' b -> utf8 ',' <> renderCell1 c' <> b) mempty cs
+    renderCell1 c <> mconcat [ B.utf8 ',' <> renderCell1 c' | c' <- cs ]
 
-renderTable2 :: Table -> Builder
-renderTable2 = 
-    -- mconcat [renderRow2 r <> utf8 '\n' | r <- rs]
-    foldr (\r b -> renderRow2 r <> utf8 '\n' <> b) mempty
+renderTable2 :: Table -> B.Builder
+renderTable2 rs = mconcat [renderRow2 r <> B.utf8 '\n' | r <- rs]
 
 encodeUtf8CSV2 :: Table -> L.ByteString
-encodeUtf8CSV2 = toLazyByteString . renderTable2
+encodeUtf8CSV2 = B.toLazyByteString . renderTable2
 
 
 ------------------------------------------------------------------------------
@@ -122,8 +156,8 @@ encodeUtf8CSV2 = toLazyByteString . renderTable2
 ------------------------------------------------------------------------------
 
 -- improved
-renderString3 :: String -> Builder
-renderString3 cs = utf8 '"' <> fromWriteList writeEscaped cs <> utf8 '"'
+renderString3 :: String -> B.Builder
+renderString3 cs = B.utf8 '"' <> B.fromWriteList writeEscaped cs <> B.utf8 '"'
   where
     writeEscaped :: Write Char
     writeEscaped = 
@@ -132,34 +166,30 @@ renderString3 cs = utf8 '"' <> fromWriteList writeEscaped cs <> utf8 '"'
       W.utf8
 
 -- carry over definitions
-renderCell3 :: Cell -> Builder
+renderCell3 :: Cell -> B.Builder
 renderCell3 (StringC cs) = renderString3 cs
-renderCell3 (IntC i)     = utf8 $ show i
+renderCell3 (IntC i)     = B.utf8 $ show i
            
-renderRow3 :: Row -> Builder
+renderRow3 :: Row -> B.Builder
 renderRow3 []     = mempty
 renderRow3 (c:cs) = 
-    renderCell3 c <> mconcat [ utf8 ',' <> renderCell3 c' | c' <- cs ]
-    -- renderCell3 c <> mconcat [ utf8 ',' <> renderCell3 c' | c' <- cs ]
+    renderCell3 c <> mconcat [ B.utf8 ',' <> renderCell3 c' | c' <- cs ]
 
-renderTable3 :: Table -> Builder
-renderTable3 = 
-    -- mconcat [renderRow3 r <> utf8 '\n' | r <- rs]
-    foldr (\r b -> renderRow3 r <> utf8 '\n' <> b) mempty
+renderTable3 :: Table -> B.Builder
+renderTable3 rs = mconcat [renderRow3 r <> B.utf8 '\n' | r <- rs]
 
 encodeUtf8CSV3 :: Table -> L.ByteString
-encodeUtf8CSV3 = toLazyByteString . renderTable3
+encodeUtf8CSV3 = B.toLazyByteString . renderTable3
 
 ------------------------------------------------------------------------------
 -- Benchmark Data
 ------------------------------------------------------------------------------
 
-n :: Int
-n = 10000
-
 {-# NOINLINE tableInput #-}
 tableInput :: [(String, Table)]
-tableInput = [ (show n, take n $ cycle table) ]
+tableInput = 
+  -- [ (show $ length table, table) 
+  [ (show n, take n $ cycle table) ]
 
 {-# NOINLINE stringInput #-}
 stringInput :: [(String, [String])]
@@ -171,11 +201,11 @@ stringInput = [ (show n, take n $ cycle strings) ]
 ------------------------------------------------------------------------------
 
 original, version1, version2, version3 
-  :: ([Char], Builder -> Int64, Builder -> L.ByteString)
+  :: ([Char], B.Builder -> Int64, B.Builder -> L.ByteString)
 (original, version1, version2, version3) =
     (f "original", f "version1", f "version2", f "version3")
   where
-    f x = (x, L.length . toLazyByteString, toLazyByteString)
+    f x = (x, L.length . B.toLazyByteString, B.toLazyByteString)
 
 benchmarks :: [Benchmark]
 testResults :: [String]
@@ -210,12 +240,40 @@ testResults :: [String]
       , map (fTest . f . snd) inputs
       )
 
+  {-
 
 main :: IO ()
 main = do
+  print $ encodeUtf8CSV table
+  print $ map S.length $ L.toChunks $ encodeUtf8CSV maxiTable
+  -}
+  {-
   putStrLn ""
   putStrLn "Comparing results of different implementations:"
   putStrLn $ unlines $ map ("  " ++) testResults
   putStrLn "Benchmarking:"
   defaultMain benchmarks
+  -}
+main :: IO ()
+main = do
+  print $ encodeUtf8CSV table
+  print $ map S.length $ L.toChunks $ encodeUtf8CSV maxiTable
+  print $ L.length $ encodeUtf8CSV maxiTable
+  print ""
+  defaultMain
+    [ bench "encodeCSV maxiTable (DList)" $ 
+        whnf (length . encodeCSV) maxiTable
+    , bench "encodeCSV maxiTable (DList + Utf8 encode)" $ 
+        whnf (L.length . encodeUtf8CSV_Dlist) maxiTable
+    , bench "encodeUtf8CSV maxiTable (original)" $ 
+        whnf (L.length . encodeUtf8CSV) maxiTable
+    , bench "encodeUtf8CSV maxiTable (version 1)" $ 
+        whnf (L.length . encodeUtf8CSV1) maxiTable
+    , bench "encodeUtf8CSV maxiTable (version 2)" $ 
+        whnf (L.length . encodeUtf8CSV2) maxiTable
+    , bench "encodeUtf8CSV maxiTable (version 3)" $ 
+        whnf (L.length . encodeUtf8CSV3) maxiTable
+    ]
+
+
 
